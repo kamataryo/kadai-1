f9c240e8e670a42dffde65b66c2feb134e96c698
satton0425 <satosato1998425@gmail.com>
2019-11-06 05:27:44 +0000

    HTMLを追加

--- kadai-3/index2.js
+++ kadai-3/index2.js
@@ -4,7 +4,7 @@ const max = parseInt(process.argv[2])
 
 for (let n = 1;n<= max;n = n + 1) {
     if (n % 3 === 0) {
-        console.log('3の倍数ですよ')
+        console.log('キュイン！')
     }　else {
         console.log(n)
     }
